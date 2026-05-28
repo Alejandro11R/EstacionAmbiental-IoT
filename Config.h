255e3dfe46701a0528eda081697237dd6ce583da
Hugo Alejandro Rodriguez <168315710+Alejandro11R@users.noreply.github.com>
2026-05-27 22:43:55 -0500

    Add configuration header file with constants

---
+++ Config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID           "Wokwi-GUEST"
+#define WIFI_PASSWORD       ""
+#define THINGSPEAK_API_KEY  "R9Y26BZ6WRSN160F"
+#define CHANNEL_ID          3395823
+
+#define PIN_LDR      34
+#define PIN_DHT       4
+#define PIN_LED       2
+#define PIN_BOTON    15
+
+#define TEMP_ALERTA   30.0
+#define INTERVALO_MS  15000
+
+#endif
